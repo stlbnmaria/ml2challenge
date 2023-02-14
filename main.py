d452e8ebfce64b945805c8fa8c520379bb85be4d
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-02-14 09:57:19 +0100

    initial setup

---
+++ main.py
@@ -0,0 +1,16 @@
+import argparse
+from pathlib import Path
+
+###############################################################################
+
+parser = argparse.ArgumentParser(description = 'Main Script to Run Training')
+parser.add_argument('--model', type = str, default = r"XGB", help = 'Type of model to run training')
+parser.add_argument('--goal', type = str, default = r"test", help = 'Goal of the run (test/submission/tuning)')
+parser.add_argument('--subname', type = str, default = r"test", help = 'Name of the submission')
+args = parser.parse_args()  
+
+###############################################################################
+
+if __name__ == "__main__":
+
+    input_args = vars(args)
